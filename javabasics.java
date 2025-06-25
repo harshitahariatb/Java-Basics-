7d09c0753b5cf7c31972c47a55adaa039760317f
Harshita Harriet <harshitaharriet05@gmail.com>
2025-06-25 15:22:40 +0530

    Add files via upload

---
+++ javabasics.java
@@ -0,0 +1,49 @@
+class Main {
+ 
+   public static void main(String[] args) {
+      
+   int a=2;int b=10;
+     
+   System.out.println(a/b);
+
+ }
+
+
+
+class Main {
+ 
+   public static void main(String[] args) {
+  
+      int a=2;int b=10;
+      
+     System.out.println(a/b);
+   
+ }
+
+
+
+class Main {
+ 
+   public static void main(String[] args) {
+  
+      int h=3;int c=25;
+
+        System.out.println(h!=c);
+
+    }
+
+ class Main {
+
+    public static void main(String[] args) {
+
+        int a=22;int b=33;
+
+        System.out.println(a++);
+
+         System.out.println(++a);
+
+          System.out.println(-a);
+
+    }
+
+}
