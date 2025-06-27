0cf6e7af6b9f8662787f1a24c476c75458267428
Harshita Harriet <harshitaharriet05@gmail.com>
2025-06-27 12:02:56 +0530

    Add files via upload

---
+++ javabasics2.java
@@ -0,0 +1,25 @@
+
+
+class Main {
+
+    public static void main(String[] args) {
+
+        int x=5;
+
+        if(x>7) {
+
+            System.out.println("Hi");
+
+        }
+        if (x<7) {
+       System.out.println("Hello");
+ 
+       }else{
+
+            System.out.println("Welcome");
+
+       }
+
+    } 
+
+}
